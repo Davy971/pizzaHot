68e8211bde17c9f6c974dc6d32075550f0266a41
Unknown <regalade.davy@gmail.com>
2018-02-20 13:40:09 -0400

    premier test user
    manuel

---
+++ src/OC/PlatformBundle/Controller/ClientController.php
@@ -0,0 +1,29 @@
+<?php
+
+// src/OC/PlatformBundle/Controller/AdvertController.php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class ClientController extends Controller
+{
+    public function ajouterAction()
+      {
+        
+      }
+      public function supprimerAction()
+      {
+
+      }
+      public function updateAction()
+      {
+
+      }
+
+      
+
+   
+
+}
